559c3f3e56bf9e6631db2fe8c1f5e6143dd8edb2
Molly Jewett <mjewett90966@uat.edu>
2025-08-17 22:19:24 -0700

    First commit

---
+++ html/sound.js
@@ -0,0 +1,6 @@
+
+                function pauseSound() {
+                    const audio = new Audio('alien-spaceship_daniel_simion (1).mp3');
+                    audio.pause();
+                }
+           
